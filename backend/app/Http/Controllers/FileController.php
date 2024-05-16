20be3c38c9b5bcf3266f14d6b82a69591026e70b
thrnkk <thomas.ricardo.reinke@gmail.com>
2024-05-15 22:25:16 -0300

    feat: add file controller validator

--- backend/app/Http/Controllers/FileController.php
+++ backend/app/Http/Controllers/FileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Validator;
 use App\Jobs\ProcessImportJob;
 use App\Models\File;
 use App\Services\FileProcessService;
@@ -25,12 +26,24 @@ class FileController extends Controller
 
     public function store(Request $request)
     {
+        $validation = Validator::make(
+            $request->all(),
+            [
+                'file_upload' => 'mimes:zip'
+            ],
+            [
+                'file_upload.mimes' => 'The file should be a .zip!'
+            ]
+        );
+
+        if ($validation->fails()) return response()->json($validation->messages(), 400);
+
         $file = $request->file('file_upload');
         $fileName = $request->input('file_name');
         $filePath = $file->store('uploads', 'public');
 
         $this->fileService->process($filePath, $fileName);
-        dispatch(new ProcessImportJob($filePath, $fileName));
+        // dispatch(new ProcessImportJob($filePath, $fileName));
 
         return response()->json([], 201);
     }
